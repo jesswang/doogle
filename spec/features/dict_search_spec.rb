c8c3143566430b64741b6620b81a23d89cdce476
Talha Saifi <instantink+pair+talha@hp.com>
2015-02-23 14:36:02 -0800

    added feature spec and added .idea/workspace.xml to .gitignore

--- spec/features/dict_search_spec.rb
+++ spec/features/dict_search_spec.rb
@@ -7,3 +7,12 @@ describe "initial view" do
     expect(page).to have_selector("#search-results", text: "")
   end
 end
+
+describe "dictionary search" do
+  it "displays definition(s) for valid search" do
+    visit '/'
+    fill_in 'search-bar', :with => 'poodle'
+    click_button 'search-button'
+    expect(page).to have_selector("li")
+  end
+end
