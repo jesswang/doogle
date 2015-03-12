529f5eae44bc1fa08b822ed0517c2b2fc2869ca9
Jessica Wang <wang336@usc.edu>
2015-03-11 17:15:42 -0700

    add save to db after dictionary api request

--- app/controllers/definitions_controller.rb
+++ app/controllers/definitions_controller.rb
@@ -7,21 +7,28 @@ class DefinitionsController < ApplicationController
   end
 
   def show
+    # Show definitions from the database if record(s) exist(s), otherwise call API
     if Definition.exists?(word: params[:id])
       response = retrieveRecordFromDatabase(word: params[:id])
     else
-      response = makeDictionaryRequest(word: params[:id])
+      response = makeDictionaryApiRequest(word: params[:id])
     end
     render json: {definitions: response}
   end
 
-  def makeDictionaryRequest(word: )
+  def makeDictionaryApiRequest(word: )
     response = Array.new
     key = "cab72891-f003-43ef-a983-253666d45082"
     request = Faraday.new.get("http://www.dictionaryapi.com/api/v1/references/collegiate/xml/#{word}?key=#{key}").body
     xml = Nokogiri::XML(request)
+
+    # extract content from <dt> XML tags
     xml.css("dt").each do |d|
-      response.push d.content[1..-1] # exclude preceding ":"
+      meaning = d.content[1..-1] # exclude preceding ":"
+      response.push meaning
+
+      # save to DB
+      Definition.create(word: word, meaning: meaning)
     end
     return response
   end
--- spec/controllers/definitions_controller_spec.rb
+++ spec/controllers/definitions_controller_spec.rb
@@ -8,12 +8,13 @@ RSpec.describe DefinitionsController, type: :controller do
       expect(response).to have_http_status(200)
     end
 
-    it "calls #makeDictionaryRequest when a record for the word does not exist in the database" do
-      expect(controller).to receive(:makeDictionaryRequest).with(word: "kiwi")
+    it "calls #makeDictionaryApiRequest when the word is not in the database" do
+      expect(controller).to receive(:makeDictionaryApiRequest).with(word: "kiwi")
       get :show, :id => "kiwi"
+      # expect(Definition.where(word: "kiwi")).to exist # why doesn't this work
     end
 
-    it "calls #retrieveRecordFromDatabase when a record for the word exists in the database" do
+    it "calls #retrieveRecordFromDatabase when the word exists in the database" do
       Definition.create(word: "kiwi", meaning: "fruit")
       expect(controller).to receive(:retrieveRecordFromDatabase).with(word: "kiwi")
       get :show, :id => "kiwi"
